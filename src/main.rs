e9754f0d0b91f5056cdb9ddf7d9ea9d2ddd29664
Simone Walter <waltersz@protonmail.com>
2022-07-15 11:57:05 +0200

    Add a scene loading and better screenshots

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,25 @@
-use std::error::Error;
-use std::path::PathBuf;
+use std::borrow::Cow;
+use std::fs::{self, File};
+use std::io::{self, BufReader, BufWriter, Write};
+use std::path::{Path, PathBuf};
 use std::thread;
 use std::time::{Duration, Instant};
 
-use bendy_tracer::scene::{Camera, Data, Material, Object, Scene, Sphere, Update, UpdateQueue};
+use anyhow::{ensure, Error};
+use bendy_tracer::scene::{Camera, Data, Material, Object, Scene, Sphere};
 use bendy_tracer::tracer::{Buffer, Status, Tracer};
 use clap::Parser;
-use glam::{Affine3A, Vec3, Vec3A};
+use glam::{Affine3A, Quat, Vec3, Vec3A};
 use sdl2::event::Event;
 use sdl2::keyboard;
 use sdl2::pixels::PixelFormatEnum;
 
 const SAMPLES_STEP: usize = 8;
+const SAMPLES_BIG_STEP: usize = 64;
+const SAMPLES_VERY_BIG_STEP: usize = 1024;
+
+const DEFAULT_SCENE: &str = "scene.json";
+const DEFAULT_SCREENSHOT: &str = "render.png";
 
 #[derive(Debug, Parser)]
 #[clap(author, version, about)]
@@ -22,14 +30,20 @@ struct Cli {
     #[clap(long, value_parser, default_value_t = 512)]
     height: u32,
 
-    #[clap(long, value_parser, default_value_t = 32)]
+    #[clap(long, value_parser, default_value_t = 64)]
     max_samples: usize,
 
     #[clap(long, value_parser, default_value_t = 2.0)]
     gamma: f32,
+
+    #[clap(long, value_parser, default_value_os_t = PathBuf::from("screenshots/render.png"))]
+    screenshot: PathBuf,
+
+    #[clap(long, value_parser)]
+    scene: Option<PathBuf>,
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() -> Result<(), Error> {
     let args = Cli::parse();
 
     let sdl_context = sdl2::init().unwrap();
@@ -45,16 +59,52 @@ fn main() -> Result<(), Box<dyn Error>> {
     canvas.clear();
     canvas.present();
 
-    let mut scene = Scene::default();
+    let mut scene = if let Some(path) = &args.scene {
+        let file = File::open(path)?;
+        let mut reader = BufReader::new(file);
+        let scene = serde_json::from_reader(&mut reader)?;
 
-    let mat_red = scene.add_data(Data::new(Material::flat(Vec3A::new(1.0, 0.0, 0.0))));
+        writeln!(io::stderr(), "loaded scene from {}", path.display())?;
 
-    let camera = scene.add_object(Object::new(Camera::default()));
-    let sphere = scene.add_object(
-        Object::new(Sphere::new(mat_red, 1.0)).with_translation(Vec3A::new(0.0, 0.0, -8.0)),
-    );
+        scene
+    } else {
+        let mut scene = Scene::default();
 
-        
+        let mat_root = scene.add_data(Data::new(Material::emissive(Vec3A::ONE, 0.1)));
+        let mat_light = scene.add_data(Data::new(Material::emissive(Vec3A::ONE, 10.0)));
+        let mat_red = scene.add_data(Data::new(Material::diffuse(Vec3A::new(0.7, 0.1, 0.1), 0.5)));
+        let mat_blue = scene.add_data(Data::new(Material::diffuse(Vec3A::new(0.2, 0.2, 0.5), 0.8)));
+
+        scene.set_root_material(mat_root);
+
+        scene.add_object(
+            Object::new(Camera::default())
+                .with_tag("camera".to_string())
+                .with_transform(Affine3A::from_rotation_translation(
+                    Quat::from_euler(
+                        glam::EulerRot::YXZ,
+                        10_f32.to_radians(),
+                        -5_f32.to_radians(),
+                        0.0,
+                    ),
+                    Vec3::new(1.6, 2.1, 8.0),
+                )),
+        );
+        scene.add_object(
+            Object::new(Sphere::new(mat_blue, 100.0))
+                .with_translation(Vec3A::new(0.0, -101.0, 0.0)),
+        );
+        scene.add_object(
+            Object::new(Sphere::new(mat_red, 1.0)).with_translation(Vec3A::new(0.0, 0.0, 0.0)),
+        );
+        scene.add_object(
+            Object::new(Sphere::new(mat_light, 0.5)).with_translation(Vec3A::new(3.0, 3.0, 2.0)),
+        );
+
+        scene
+    };
+
+    let camera = scene.find_by_tag("camera").unwrap();
 
     let tracer = Tracer::new();
 
@@ -66,11 +116,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         .unwrap();
 
     let mut event_pump = sdl_context.event_pump().unwrap();
-    let first_frame = Instant::now();
     let mut prev_frame = Instant::now();
-
-    let mut speed = 0.0;
-    let mut velocity;
 
     'main: loop {
         if tracer.render_samples(&scene, camera, 1, &mut buffer) == Status::Rendered {
@@ -82,40 +128,45 @@ fn main() -> Result<(), Box<dyn Error>> {
             canvas.copy(&texture, None, None).unwrap();
         }
 
-        // time of last render, not the entire loop
+        // delta time of the render, not the entire loop
         let this_frame = Instant::now();
         let delta = this_frame - prev_frame;
-        let time = this_frame - first_frame;
 
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit { .. } => break 'main,
                 Event::KeyDown {
-                    keycode: Some(keyboard::Keycode::D),
-                    repeat: false,
-                    ..
-                } => speed += 0.1,
-                Event::KeyDown {
-                    keycode: Some(keyboard::Keycode::A),
-                    repeat: false,
-                    ..
-                } => speed -= 0.1,
-                Event::KeyDown {
                     keycode: Some(keyboard::Keycode::Q),
+                    keymod,
                     ..
                 } => {
-                    let max_samples = buffer.max_samples().saturating_sub(SAMPLES_STEP);
+                    let step = if keymod.contains(keyboard::Mod::LSHIFTMOD) {
+                        SAMPLES_VERY_BIG_STEP
+                    } else if keymod.contains(keyboard::Mod::LCTRLMOD) {
+                        SAMPLES_BIG_STEP
+                    } else {
+                        SAMPLES_STEP
+                    };
+                    let max_samples = buffer.max_samples().saturating_sub(step);
                     buffer.set_max_samples(max_samples.max(1));
                     buffer.clear();
                 }
                 Event::KeyDown {
                     keycode: Some(keyboard::Keycode::E),
+                    keymod,
                     ..
                 } => {
-                    let max_samples = if buffer.max_samples() == 1 {
-                        SAMPLES_STEP
+                    let step = if keymod.contains(keyboard::Mod::LSHIFTMOD) {
+                        SAMPLES_VERY_BIG_STEP
+                    } else if keymod.contains(keyboard::Mod::LCTRLMOD) {
+                        SAMPLES_BIG_STEP
                     } else {
-                        buffer.max_samples() + SAMPLES_STEP
+                        SAMPLES_STEP
+                    };
+                    let max_samples = if buffer.max_samples() == 1 {
+                        step
+                    } else {
+                        buffer.max_samples() + step
                     };
                     buffer.set_max_samples(max_samples);
                     buffer.clear();
@@ -126,22 +177,67 @@ fn main() -> Result<(), Box<dyn Error>> {
                     keymod,
                     ..
                 } if keymod.contains(keyboard::Mod::LCTRLMOD) => {
-                    buffer.preview_or_update().save(&args.path)?
+                    let path = &args.screenshot;
+                    let path = if path.extension().is_none() {
+                        Cow::from(path.with_file_name(DEFAULT_SCREENSHOT))
+                    } else {
+                        Cow::from(path)
+                    };
+
+                    if let Some(dir) = path.parent() {
+                        if dir.is_dir() {
+                            ensure!(
+                                !dir.exists(),
+                                "{dir} is not a directory",
+                                dir = dir.display(),
+                            );
+                        } else {
+                            fs::create_dir_all(dir)?;
+                        }
+                    }
+
+                    buffer.preview_or_update().save(&path)?;
+
+                    writeln!(io::stderr(), "saved screenshot to {}", path.display())?;
+                }
+                Event::KeyDown {
+                    keycode: Some(keyboard::Keycode::K),
+                    repeat: false,
+                    keymod,
+                    ..
+                } if keymod.contains(keyboard::Mod::LCTRLMOD) => {
+                    let path = args
+                        .scene
+                        .as_deref()
+                        .unwrap_or_else(|| Path::new(DEFAULT_SCENE));
+
+                    let file = File::create(path)?;
+                    let mut writer = BufWriter::new(file);
+                    serde_json::to_writer_pretty(&mut writer, &scene)?;
+
+                    writeln!(io::stderr(), "saved scene to {}", path.display())?;
+                }
+                Event::KeyDown {
+                    keycode: Some(keyboard::Keycode::L),
+                    repeat: false,
+                    keymod,
+                    ..
+                } if keymod.contains(keyboard::Mod::LCTRLMOD) => {
+                    let path = args
+                        .scene
+                        .as_deref()
+                        .unwrap_or_else(|| Path::new(DEFAULT_SCENE));
+
+                    let file = File::open(path)?;
+                    let mut reader = BufReader::new(file);
+                    scene = serde_json::from_reader(&mut reader)?;
+                    buffer.clear();
+
+                    writeln!(io::stderr(), "loaded scene from {}", path.display())?;
                 }
                 _ => {}
             }
         }
-
-        velocity = time.as_secs_f32().cos();
-
-        update_queue.push(Update::object(sphere, move |sphere, update_queue| {
-            sphere.apply_transform(
-                update_queue,
-                Affine3A::from_translation(Vec3::X * velocity * speed * delta.as_secs_f32()),
-            );
-        }));
-
-        update_queue.commit(&mut scene);
 
         let samples = buffer.samples();
         let max_samples = buffer.max_samples();
